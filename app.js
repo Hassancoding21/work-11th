f58ccce5cb667c0cf485f02373b98a8c17d2a06d
Hassancoding21 <hhaassaann2100@gmail.com>
2024-05-08 16:13:40 +0500

    added app.js and index.html

---
+++ app.js
@@ -0,0 +1,154 @@
+// let country = prompt("tell their country")
+// let city = prompt("tell about your city")
+// let area = prompt("tell about their area")
+
+
+  
+// if(country=== "pakistan" && city=== "karachi" && area=== "defence")
+//     console.log("welcome")
+// else{
+//     console.log("invalid area")
+// }
+
+// 2nd program
+
+// let need = prompt("you want coffee or tea")
+
+// need=need.toLowerCase();
+// if(need==="tea"){
+//     console.log("here is your tea")
+// }
+
+// else if(need=== "coffee"){
+//     console.log("here is your coffee")
+// }
+// else{
+//     console.log("something went wrong")
+// }
+
+// 3rd program
+
+// let membership = prompt("you have a membership (YES/NO)").toLowerCase();
+
+// let student = prompt("you are a student (YES/NO)").toLowerCase();
+
+// if(membership === "yes" || student === "yes") {
+//     console.log("you get discount");
+// }
+// else {
+//     console.log("you dont get discount");
+// }
+
+// 4th Program
+
+
+// let age = prompt("enter your age").toLowerCase();
+// let id = prompt("you have valid id (yes/no)").toLowerCase();
+// if(age === "18" && id === "yes") {
+//     console.log("you are eligible");
+// }
+// else{
+//     console.log("you are not eligible");
+// }
+
+
+// 5th program
+
+// let fatherPaying = false;
+// let sale = false;
+
+// if (fatherPaying || sale) {
+//     console.log("You will go shopping.");
+// } else {
+//     console.log("You will not go shopping.");
+// }
+
+// 6th program
+
+// let car = prompt("you have owm car").toLowerCase();
+// let age = prompt("whats your age").toLowerCase();
+// if(age === "17" && car === "yes"){
+//     console.log("you can drive and you own a car")
+// }
+// else{
+//     console.log("you cant drive a car")
+// }
+
+// 7th  program
+
+// let dayNumber = parseInt(prompt("Enter a number representing a day of the week"));
+
+
+// if (dayNumber === 1) {
+//     console.log("Monday");
+// } else if (dayNumber === 2) {
+//     console.log("Tuesday");
+// } else if (dayNumber === 3) {
+//     console.log("Wednesday");
+// } else if (dayNumber === 4) {
+//     console.log("Thursday");
+// } else if (dayNumber === 5) {
+//     console.log("Friday");
+// } else if (dayNumber === 6) {
+//     console.log("Saturday");
+// } else if (dayNumber === 7) {
+//     console.log("Sunday");
+// } else {
+//     console.log("Invalid input. Please enter a number between 1 and 7.");
+// }
+
+//    8th program 
+
+// let colour = prompt("enter a colour").toLowerCase();
+ 
+
+// if(colour === "red"){
+//     console.log("stop!")
+// }
+// else if (colour === "yellow"){
+//     console.log("slow down")
+// }
+// else if (colour === "green"){
+//     console.log("GO")
+// }
+// else{
+//     console.log("invalid output")
+// }
+
+// 9th program
+
+// let daynum = prompt("enter day").toLowerCase();
+
+// if (daynum === "sunday"){
+
+
+//     console.log("its a weekend")
+// }
+// else if(daynum === "tuesday"){
+//    console.log("its a week day")
+// }
+// else{
+//     console.log("invalid input")
+// }
+
+// 10th program
+
+// let age = prompt("enter age");
+// if(age >1 && age <= 12){
+//     console.log("child")
+// }
+// else if (age >12 && age<= 18){
+//     console.log("teenager")
+// }
+// else if (age >18 && age <=65){
+//   console.log("adult")
+// }
+// else if (age > 65 && age <150){
+//     console.log("senior")
+// }
+// else if (age === "1" || age === "150"){
+//     console.log("invalid age")
+// }
+// else {
+//     console.lo("something went wrong")
+// }
